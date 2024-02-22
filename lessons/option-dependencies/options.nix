b147d857fc455cf5066ab72a762c575fdafc44f2
Dan Baker <daniel.n.baker@gmail.com>
2024-02-21 19:16:23 -0800

    lesson/add option dependencies (#21)
    * Started a new lesson about option dependencies and using the config value of one option in another.
    
    * Writing up more of the lesson for option dependencies.
    
    * Fix typo.
    
    * Removed the default value for greeting and switched to using config instead. Discussion fo default can be done later with better focus.

---
+++ lessons/option-dependencies/options.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config;
+in {
+  options = {
+    name = lib.mkOption {
+      type = lib.types.str;
+    };
+    title = lib.mkOption {
+      type = lib.types.str;
+    };
+    origin = lib.mkOption {
+      type = lib.types.str;
+    };
+    greeting = lib.mkOption {
+      type = lib.types.str;
+    };
+  };
+
+  config = {
+    greeting = ''
+      Hello
+      My name is ${cfg.name}.
+      I am a ${cfg.title}.
+      I am from ${cfg.origin}.'';
+  };
+}
